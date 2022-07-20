5f41c048851bc12866ba1f2abf191556d02b4486
rajib <rajibislam700@gmail.com>
2022-07-20 22:32:15 +0600

    ccrt-signup:create data folder

---
+++ data/signup/data.js
@@ -0,0 +1,10 @@
+export const TITLE = "CCRT BANGLADESH";
+export const SUBTITLE = `CCRT stands for cancer care and research trust. CCRT’s objectives are
+          to provide cancer patients with supportive care & information, and to
+          raise and disburse funds for cancer researchers in bangladesh.`;
+export const SIGN_UP_TITLE = "Create Account";
+export const SIGN_UP_BUTTON = "Sign Up";
+export const TERMS_CONDITIONS =
+  "I agree to the Terms of Service and Privacy Policy";
+export const SIGN_UP_WITH_GOOGLE = "Signup with google";
+export const SIGN_UP_WITH_FACEBOOK = "Signup with facebook";
