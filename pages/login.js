613bcaa500172e7ec6d1677845bb5ab2814c99e1
rajib <rajibislam700@gmail.com>
2022-07-20 22:33:30 +0600

    ccrt-signup:create login page

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const login = () => {
+  return (
+    <Grid>
+      <h2>Login</h2>
+    </Grid>
+  );
+};
+
+export default login;
