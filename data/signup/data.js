bc6fc6adec5907a931fd1a304195fb8d52e8b410
rajib <rajibislam700@gmail.com>
2022-07-23 10:06:55 +0600

    ccrt-signup:add error data

--- data/signup/data.js
+++ data/signup/data.js
@@ -8,3 +8,10 @@ export const TERMS_CONDITIONS =
   "I agree to the Terms of Service and Privacy Policy";
 export const SIGN_UP_WITH_GOOGLE = "Signup with google";
 export const SIGN_UP_WITH_FACEBOOK = "Signup with facebook";
+export const formErrors = {
+  name: "Name should contain only english alphabet, -, _ and whitespace",
+  email: "Email address is not valid",
+  password: "Password must be at-least 6 characters long",
+  confirmPassword: "Passwords haven't matched",
+  policy: "You have to accept our policy to create an account.",
+};
