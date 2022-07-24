562b625dfe90563d50d2f7ce8ccac99f567e6855
rajib <rajibislam700@gmail.com>
2022-07-24 10:06:42 +0600

    ccrt-login:create login screen stylesheet

---
+++ styles/loginstyles.js
@@ -0,0 +1,10 @@
+import { createStyles, makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles((theme) =>
+  createStyles({
+    ccrt__login__container: {
+      background: "red",
+      width: "50%",
+    },
+  })
+);
