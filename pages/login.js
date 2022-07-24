86124df8487682837826e1745247b0a10fb506ce
rajib <rajibislam700@gmail.com>
2022-07-24 10:06:01 +0600

    ccrt-login:create login screen

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,26 @@
-import { Grid } from "@mui/material";
 import React from "react";
+import Head from "next/head";
+import { Grid } from "@mui/material";
+import { useStyles } from "../styles/loginstyles";
 
 const login = () => {
+  const classes = useStyles();
+
   return (
-    <Grid container>
-      <h2>Login</h2>
-    </Grid>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid
+          container
+          className={classes.ccrt__login__container}
+          // style={{ background: "red", width: "50%" }}
+        >
+          <h2>Login</h2>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
